f8269249c599375d61eabd62f722c65767bd1cc8
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-08-02 00:07:16 +0400

    base services and better bash script

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Réseau Orion
+
+> Orion is a network for communicating between IT students. It uses WireGuard and BGP to build a mesh-like network where everyone can publish IPs, DNS domains and L4+ services.
+
+Structure of a orion packet:
+
+```
++----------------+
+|    IP Packet   |
++----------------+
+|    GRE         |
++----------------+
+|    IP          |
++----------------+
+|    WireGuard   |
++----------------+
+|    IP          |
++----------------+
+|    Ethernet    |
++----------------+
+```
+
+Each participant of the network can publish ips or simply exist in the network.
--- iptables.sh
+++ iptables.sh
@@ -5,15 +5,13 @@ ipset create orion-routed hash:net
 ipset add orion-routed 10.30.0.0/16
 ipset add orion-routed 172.16.0.0/15
 
+
+
 # Create a subchain
 iptables -N ext-orion
 
 # We accept incoming traffic from orion.
 iptables -A ext-orion -m set --match-set orion-routed dst -j ACCEPT
-
-# If you have a service that is manually exposed
-# You need to allow forwarding to it.
-ipset add orion-routed 192.168.1.40/32
 
 # Allow conns that wera already established.
 iptables -A ext-orion -m state --state ESTABLISHED -j ACCEPT
@@ -25,13 +23,22 @@ iptables -A FORWARD -j ext-orion
 iptables -A FORWARD -j DROP
 
 # Traffic to orion that is not in an orion subnet should be nat translated
+# This uses the 10.30.1.1 ip for all outoing connections.
 iptables -t nat -A POSTROUTING \
     -m set ! --match-set orion-routed src \
     -m set --match-set orion-routed dst \
-    -j MASQUERADE
+    -j SNAT --to-source 10.30.1.1
 
-# We translate traffic destinated to our http service to the internal ip
-# In this case, we expose our service to 10.30.1.1/32
-iptables -t nat -A PREROUTING \
-    -d 10.30.1.1/32 \
-    -j DNAT --to-destination 192.168.1.40
+FC=`cat orion_services`
+
+for kv in $"${FC[@]}" ; do
+    ORION_IP=${kv%%:*}
+    LOCAL_IP=${kv#*:}
+
+    # Add to the allowed routed
+    ipset add orion-routed $ORION_IP/32
+
+    iptables -t nat -A PREROUTING \
+        -d $ORION_IP/32 \
+        -j DNAT --to-destination $LOCAL_IP
+done
