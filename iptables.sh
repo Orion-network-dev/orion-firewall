93c134fcc0abd5c7af0ee153322d3d36c3f5044a
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-08-02 15:52:03 +0400

    fix iptables

--- iptables.sh
+++ iptables.sh
@@ -1,17 +1,20 @@
 ## Build iptables rules
 
 # Setting up ipsets
-ipset add orion-routed 10.30.0.0/16
-ipset add orion-routed 172.16.0.0/15
+
+ipset add orion-net 10.30.0.0/16
+ipset add orion-net 172.16.0.0/15
 
 # Create a subchain
 iptables -N ext-orion
 
-# We accept incoming traffic from orion.
+# Accept traffic from the forwarded ips
 iptables -A ext-orion -m set --match-set orion-routed dst -j ACCEPT
-
+# We accept incoming traffic from orion.
+iptables -A ext-orion -m set --match-set orion-net dst -j ACCEPT
 # Allow conns that wera already established.
 iptables -A ext-orion -m state --state ESTABLISHED -j ACCEPT
+# We drop by default
 iptables -A ext-orion -j DROP
 
 # We apply our rule to all the orion interfaces.
@@ -22,8 +25,9 @@ iptables -A FORWARD -j DROP
 # Traffic to orion that is not in an orion subnet should be nat translated
 # This uses the 10.30.1.1 ip for all outoing connections.
 iptables -t nat -A POSTROUTING \
-    -m set ! --match-set orion-routed src \
-    -m set --match-set orion-routed dst \
+    -m set ! --match-set orion-net src \
+    -m set --match-set orion-net dst \
+    -m set ! --match-set orion-routed dst \
     -j SNAT --to-source 10.30.1.1
 
 # In order to deploy a service, you simply need to add theses two lines
@@ -33,5 +37,4 @@ iptables -t nat -A POSTROUTING \
 # ipset add orion-routed $LOCAL_IP/32
 # iptables -t nat -A PREROUTING \
 #     -d $ORION_IP/32 \
-#     -p tcp \
 #     -j DNAT --to-destination $LOCAL_IP
