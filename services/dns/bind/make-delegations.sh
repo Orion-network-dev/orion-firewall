e589b0531b2916cf65164e0684d2da93f3310046
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-08-22 23:08:52 +0400

    fix

--- services/dns/bind/make-delegations.sh
+++ services/dns/bind/make-delegations.sh
@@ -8,10 +8,9 @@ do
 
 cat << EOF >>forwards.conf
 zone "$i.orionet.re." {
-  type forward;
-  forward only;
-  forwarders {
-    10.30.$i.255;
+  type slave;
+  masters {
+     10.30.$i.255;
   };
 };
 EOF
