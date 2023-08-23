4c6747560c0bb0d9e6d2570624f95913f4aa4d31
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-08-23 09:23:20 +0400

    dnssec algorithm change

--- services/dns/zones/genkeys.sh
+++ services/dns/zones/genkeys.sh
@@ -1,3 +1,3 @@
-dnssec-keygen -a NSEC3RSASHA1 -b 2048 -n ZONE orionet.re
-dnssec-keygen -f KSK -a NSEC3RSASHA1 -b 4096 -n ZONE orionet.re
+dnssec-keygen -a RSASHA512 -b 2048 -n ZONE orionet.re
+dnssec-keygen -f KSK -a RSASHA512 -b 4096 -n ZONE orionet.re
 
