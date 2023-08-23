95ace49e03ff7138bdea719ca46ea453aa166e74
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-08-23 09:32:03 +0400

    change signature

--- services/dns/zones/genkeys.sh
+++ services/dns/zones/genkeys.sh
@@ -1,3 +1,3 @@
-dnssec-keygen -a RSASHA512 -b 2048 -n ZONE orionet.re
-dnssec-keygen -f KSK -a RSASHA512 -b 4096 -n ZONE orionet.re
+dnssec-keygen -a ECDSA384 -b 2048 -n ZONE orionet.re
+dnssec-keygen -f KSK -a ECDSA384 -b 4096 -n ZONE orionet.re
 
