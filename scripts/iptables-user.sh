531bed3898b3c6dd25198b0422e7fd637988c35a
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-08-28 12:18:02 +0400

    secure iptables

--- scripts/iptables-user.sh
+++ scripts/iptables-user.sh
@@ -2,10 +2,12 @@
 function route_ip {
     ipset add orion-routed $2/32
     iptables -t nat -A POSTROUTING -s $2 -m devgroup --dst-group 2 -j SNAT --to-source $1
-    iptables -A ext-orion \
+    
+    # Accept incoming packets from orion interfaces by allowing using the ext-orion chain
+    echo ${@:3} | xargs iptables -A ext-orion \
         -d $2 \
         -j ACCEPT
-
+    # Rewrite packets to the destination host.
     echo ${@:3} | xargs iptables -t nat -A PREROUTING \
         -d $1/32 \
         -j DNAT --to-destination $2
