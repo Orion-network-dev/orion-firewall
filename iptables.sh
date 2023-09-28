ff9b7366075f32beeceb46a35057009c73702ccc
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-09-28 13:26:37 +0400

    fix orion rules

--- iptables.sh
+++ iptables.sh
@@ -25,14 +25,20 @@ iptables -A FORWARD \
 
 iptables -t nat -A POSTROUTING \
     -m set ! --match-set orion-net src \
-    -d 10.30.0.0/16 \
+    -m set --match-set orion-net dst \
     -m devgroup --dst-group 2 \
     -j SNAT --to-source 10.30.$1.1 \
     -m comment --comment "Packets with dst in orion-net which does not have a orion-net src and have a orion-interface routing target"
 
 iptables -t nat -A POSTROUTING \
     -m set --match-set orion-routed dst \
-    ! -s 10.30.0.0/16 \
+    -m set ! --match-set orion-net src \
     -m devgroup ! --dst-group 2 \
     -j SNAT --to-source 10.30.$1.1 \
     -m comment --comment "Packets which are dst to a locally-routed orion ip but do not have a orion-net ip and are not destinated to a orion-interface"
+
+iptables -t nat -A POSTROUTING \
+    -d 172.30.0.0/15 \
+    -d 10.30.0.0/16 \
+    -m devgroup --dst-group 2 \
+    -j SNAT --to-source 10.30.$1.1
