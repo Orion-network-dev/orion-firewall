99e69bf4e40f26e53e19fd1caa12ec5ca7a7057b
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-07-16 23:30:38 +0400

    update orion to orionv3

--- scripts/iptables-user.sh
+++ scripts/iptables-user.sh
@@ -2,7 +2,7 @@
 # Used like so: route_ip "<orion ip>" "<local ip>" <(optional) iptables options>
 function route_ip {
     ipset add orion-routed $2/32
-    iptables -t nat -A POSTROUTING -s $2 -m devgroup --dst-group 2 -j SNAT --to-source $1
+    iptables -t nat -A POSTROUTING -s $2 -m devgroup --dst-group 30 -j SNAT --to-source $1
     
     # Accept incoming packets from orion interfaces by allowing using the ext-orion chain
     echo ${@:3} | xargs iptables -A ext-orion \
