08f7ef477d3754c810037bba2470958a21863c07
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-07-17 12:47:43 +0400

    fixes-

--- iptables.sh
+++ iptables.sh
@@ -21,25 +21,22 @@ iptables -A ext-orion \
 
 iptables -A FORWARD \
     -m devgroup --src-group 30 \
-    -j ext-orion \
-    -m comment --comment "Allow forwarding given the forwarding rules for Orion"
+    -j ext-orion
 
 iptables -t nat -A POSTROUTING \
     -m set ! --match-set orion-net src \
     -m set --match-set orion-net dst \
-    -m devgroup --src-group 30 \
-    -j SNAT --to-source 10.30.$1.1 \
-    -m comment --comment "Packets with dst in orion-net which does not have a orion-net src and have a orion-interface routing target"
+    -m devgroup --dst-group 30 \
+    -j SNAT --to-source 10.30.$1.1
 
 iptables -t nat -A POSTROUTING \
     -m set --match-set orion-routed dst \
     -m set ! --match-set orion-net src \
-    -m devgroup ! --src-group 30 \
-    -j SNAT --to-source 10.30.$1.1 \
-    -m comment --comment "Packets which are dst to a locally-routed orion ip but do not have a orion-net ip and are not destinated to a orion-interface"
+    -m devgroup ! --dst-group 30 \
+    -j SNAT --to-source 10.30.$1.1
 
 iptables -t nat -A POSTROUTING \
     -s 172.30.0.0/15 \
     -d 10.30.0.0/16 \
-    -m devgroup --src-group 30 \
+    -m devgroup --dst-group 30 \
     -j SNAT --to-source 10.30.$1.1
