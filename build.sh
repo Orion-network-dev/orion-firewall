29c09153e7d0423aca3d2f3766ca416c4fffe756
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-07-19 20:34:32 +0400

    fix string substitution in build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 # Permet la génération des paquets debian
 TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
-VERSION="${TAG/-/~/X}"
+VERSION="${TAG/-/"~"}"
 fpm -t deb -v "$VERSION"
