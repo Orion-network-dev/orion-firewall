c08051d0515376129c7bd9742e2bd16ca4f2ed61
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-07-19 17:49:53 +0400

    updating the firewall scripts

--- iptables-user.example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. scripts/iptables-user.sh
-
-# Exposer un serveur (tout les ports acceptés)
-route_ip "10.30.1.1" "192.168.1.1"
-# Exposer un serveur seulement sur le port 80
-route_ip "10.30.1.2" "192.168.1.2" -p tcp --dport 80
