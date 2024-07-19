9b3a8ea6ad3ca8abf57a26d70ba4951805f670ae
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-07-19 21:07:56 +0400

    removing the services

--- services/platform/routinator/get-tals.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p tals/
-
-# Downloads the orion rpki root tal
-curl https://rpki.orionet.re/ta/ta.tal -o tals/ta.tal
-
--- services/platform/sip-proxy/make-keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo -n "aes.key = \"" > kamailio/aes_conf.cfg
-openssl rand -base64 256 | openssl base64 -A >> kamailio/aes_conf.cfg
-echo "\"" >> kamailio/aes_conf.cfg
